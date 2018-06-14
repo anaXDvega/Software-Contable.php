2486474533757b1bee643b0abae553a7bbe60719
anaXDvega <40269205+anaXDvega@users.noreply.github.com>
2018-06-14 08:20:54 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>software contable</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1, maximun-scale=1, minium-scsale=1"> 
+	<link rel="stylesheet"  href="css/fontello.css">
+	<link rel="stylesheet"  href="css/estilo_menu.css">
+</head>
+<body>
+
+
+
+<header>
+    <div class="cont">
+        <img class="logo" src="imagenes/freenet.PNG">
+        <input type="checkbox" id="menu-bar">
+        <label class="icon-menu" for="menu-bar"></label>
+                <nav class="menu">
+                    <a href="">Introduccion</a>
+                    <a href="">quienes somos</a>
+                    <a href="">contactanos</a>
+                    <a href="http://ingjavierbuitrago.com/freecount/app/login.php">ingresar</a>
+                </nav>
+            </div>
+</header>
+<main>
+            <section id="banner">
+                <background= img src="imagenes/fondo_inicio.jpg">
+                <div class="contenedor">
+                    <h2>Mascotas y niños felices</h2>
+                    <p>¿Cuál es la mejor mascota para usted?</p>
+                    <a href="#">Leer más</a>
+                </div>
+            </section>
+            
+            <section id="bienvenidos">
+            <div class="contenedor">
+                <h2>BIENVENIDOS A NUESTRO CLUB</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem ea eum alias reiciendis, tempore fugiat, distinctio nihil corrupti hic nostrum!</p>
+                </div>
+            </section>
+</body>
+</html> 
